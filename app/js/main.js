7685dc149d12c92289b56636504645aa6039f66a
Matt Brown <matt388@gmail.com>
2017-03-23 16:03:49 -0700

    styling

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,56 @@
-console.log('yes');
-console.log('no');
-console.log('maybe');
-console.log('possibly');
-console.log('dunno');
+$(document).ready(function(){
+
+	$('header').addClass(' load');
+	$('.scroll').addClass(' load');
+
+
+		$(window).scroll(function() {
+	        if ($(window).scrollTop() + $(window).height() >= $('.aboutcheck').offset().top-100) {
+	        	$('.aboutcheck').parent().addClass(' slide');
+	        }
+    	});
+    	$(window).scroll(function() {
+	        if ($(window).scrollTop() + $(window).height() >= $('.workcheck').offset().top-100) {
+	        	$('.workcheck').parent().addClass(' slide');
+	        }
+    	});
+    	$(window).scroll(function() {
+	        if ($(window).scrollTop() + $(window).height() >= $('.studycheck').offset().top-100) {
+	        	$('.studycheck').parent().addClass(' slide');
+	        }
+    	});
+    	$(window).scroll(function() {
+	        if ($(window).scrollTop() + $(window).height() >= $('.contactcheck').offset().top-100) {
+	        	$('.contactcheck').parent().addClass(' slide');
+	        }
+    	});
+
+
+	
 
 
 
+	var myUrl = 'https://teamtreehouse.com/walterbrown2.json';
+	var badgeHTML = '';
+	$.getJSON(myUrl)
+	.done(function(data){
+		badgeList = data.badges;
+		var points = data.points.total;
+		var myBadges = badgeList.length-1;
+		badgeList =  badgeList.reverse();
+		for (var i = 0; i < 5; i++) {
+			var badgeName = badgeList[i].name;
+			var badgeIcon = badgeList[i].icon_url;
+			var badgeLink = badgeList[i].url;
+
+			badgeHTML += "<a href="+badgeLink+"><img src="+badgeIcon+" class='th-badge'></a>";
+
+		$('#badges').html(badgeHTML);
+		$('#lessons').html(myBadges);
+		$('#points').html(points);
+		}
+	});
+
+
+});
 
